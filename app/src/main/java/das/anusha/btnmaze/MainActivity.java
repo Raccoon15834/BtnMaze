cf32cf8177e77106ab7f64735fbdf21afdbfc100
Raccoon15834 <anusha.das4fx@gmail.com>
2021-09-14 14:08:34 -0400

    Initial commit

---
+++ app/src/main/java/das/anusha/btnmaze/MainActivity.java
@@ -0,0 +1,21 @@
+package das.anusha.btnmaze;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //TODO: fix colors, txt color, dimens, and text labls
+        //TODO: layout 7 and 8
+        //7 layout fragments maze game. will cycle through?, dynamic scroll view, timer?
+
+        //SUBMIT: P3L04aDasAnushaLayouts.apk
+        //7 srnshots of layouts, 7 layout.xml
+        //video, apk of embellishmetns
+    }
+}
