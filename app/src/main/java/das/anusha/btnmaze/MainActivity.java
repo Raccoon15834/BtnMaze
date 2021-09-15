fa83943c086856dfb2ea2de8ddf319dc39c364e1
Raccoon15834 <anusha.das4fx@gmail.com>
2021-09-14 21:43:21 -0400

    set content view, all layouts in array

--- app/src/main/java/das/anusha/btnmaze/MainActivity.java
+++ app/src/main/java/das/anusha/btnmaze/MainActivity.java
@@ -3,19 +3,33 @@ package das.anusha.btnmaze;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
-
+    int[] layoutNums;
+    int count=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        //TODO: fix colors, txt color, dimens, and text labls
-        //TODO: layout 7 and 8
-        //7 layout fragments maze game. will cycle through?, dynamic scroll view, timer?
+        setContentView(R.layout.layout1);
 
-        //SUBMIT: P3L04aDasAnushaLayouts.apk
-        //7 srnshots of layouts, 7 layout.xml
-        //video, apk of embellishmetns
+        layoutNums = new int[]{R.layout.layout1,R.layout.layout2,R.layout.layout3,
+                R.layout.layout4,R.layout.layout5,R.layout.layout6,R.layout.layout7};
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if(event.getAction()==MotionEvent.ACTION_DOWN){
+            count = (++count)%layoutNums.length;
+            setContentView(layoutNums[count]);
+        }
+        return true;
     }
 }
+//TODO: fix colors, txt color, dimens, and text labls
+//TODO: 3:37 of vid 8 activities and intents
+//7 layout fragments maze game. will cycle through?, dynamic scroll view, timer?
+
+//SUBMIT: P3L04aDasAnushaLayouts.apk
+//7 srnshots of layouts, 7 layout.xml
+//video, apk of embellishmetns
