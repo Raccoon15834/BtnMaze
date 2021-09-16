3bbc38c7dc1802f43ade79b9b0f1cf225b1d6309
Raccoon15834 <anusha.das4fx@gmail.com>
2021-09-16 13:59:09 -0400

    needs intents and timers

--- app/src/main/java/das/anusha/btnmaze/MainActivity.java
+++ app/src/main/java/das/anusha/btnmaze/MainActivity.java
@@ -7,7 +7,6 @@ import android.view.MotionEvent;
 
 public class MainActivity extends AppCompatActivity {
     int[] layoutNums;
-    int count=-1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,18 +16,10 @@ public class MainActivity extends AppCompatActivity {
                 R.layout.layout4,R.layout.layout5,R.layout.layout6,R.layout.layout7};
     }
 
-    @Override
-    public boolean onTouchEvent(MotionEvent event) {
-        if(event.getAction()==MotionEvent.ACTION_DOWN){
-            count = (++count)%layoutNums.length;
-            setContentView(layoutNums[count]);
-        }
-        return true;
-    }
+
 }
-//TODO: fix colors, txt color, dimens, and text labls
 //TODO: 3:37 of vid 8 activities and intents
-//7 layout fragments maze game. will cycle through?, dynamic scroll view, timer?
+//on create fills scroll view with buttons, timer?
 
 //SUBMIT: P3L04aDasAnushaLayouts.apk
 //7 srnshots of layouts, 7 layout.xml
